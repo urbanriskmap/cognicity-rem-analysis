fa7c3faf8d533c558d15939afc20c433bb4a31b6
Abraham Quintero <abrahamquintero2323@gmail.com>
2017-06-23 08:00:56 -0400

    fixed coordinates, got rid of test data in source

--- src/client/app/map/navigator/navigator.component.ts
+++ src/client/app/map/navigator/navigator.component.ts
@@ -15,71 +15,9 @@ export class NavigatorComponent {
   private map: any;
 
   ngAfterContentInit() {
-    var testData = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "properties": {
-                    "epoch": "1426952605000"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        7.582512743,
-                        51.933292258,
-                        1
-                    ]
-                }
-            },
-            {
-                "type": "Feature",
-                "properties": {
-                    "epoch": "1426952605000"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        7.602516645,
-                        51.94962073,
-                        1
-                    ]
-                }
-            },
-            {
-                "type": "Feature",
-                "properties": {
-                    "epoch": "1426952609000"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        7.61132039,
-                        51.967614681,
-                        1
-                    ]
-                }
-            },
-            {
-                "type": "Feature",
-                "properties": {
-                    "epoch": "1426952608000"
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        7.595284208,
-                        51.976391375,
-                        1
-                    ]
-                }
-            }
-        ]
-    }
-
-    var map = L.map("mapContainer", {
+    let map = L.map("mapContainer", {
       zoomControl: false,
-      center: L.latLng(6.1751, 106.8650),
+      center: L.latLng(-6.1751, 106.8650),
       zoom: 12,
       minZoom: 4,
       maxZoom: 19,
