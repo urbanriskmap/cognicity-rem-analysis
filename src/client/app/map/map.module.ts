002198e7fcd3dc6143d89480671b0a3f480216a6
abrahamq <abrahamquintero2323@gmail.com>
2017-07-11 14:14:15 -0400

    leaflet markers working in prod via hack

--- src/client/app/map/map.module.ts
+++ src/client/app/map/map.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { MapComponent } from './map.component';
 import { NavigatorComponent } from './navigator/navigator.component';
-import { TimelineComponent } from './timeline/timeline.component';
+import { TimelineModule } from './timeline/timeline.module';
 import { MapRoutingModule } from './map-routing.module';
 
 @NgModule({
-  imports: [HttpModule, CommonModule, MapRoutingModule],
-  declarations: [MapComponent, NavigatorComponent, TimelineComponent],
+  imports: [HttpModule, CommonModule, MapRoutingModule, TimelineModule],
+  declarations: [MapComponent, NavigatorComponent],
   exports: [MapComponent]
 })
 export class MapModule { }
